ce017e340542b4750da381994edc6467475e2e38
mikolajqc <mikolajqc@gmail.com>
2016-05-19 21:12:11 +0200

    adding new menu

--- src/applicationState/AStateMenu.java
+++ src/applicationState/AStateMenu.java
@@ -2,9 +2,13 @@ package applicationState;
 
 import ufotron.*;
 import gui.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.lwjgl.util.vector.Vector2f;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.gui.TextField;
 
 public class AStateMenu extends ApplicationState
@@ -12,17 +16,23 @@ public class AStateMenu extends ApplicationState
 	private Button hostButton;
 	private Button joinButton;
 	
+	private Image titleImage;
+	
 	public static TextField iPServer;
 	
 	
 	@Override
 	public void Init()
 	{
-		System.out.println("Menu");
-		UfoTron.isSingleGame = false;
-		hostButton = new Button("button.png", new Vector2f(270, 190), new Vector2f(100, 100), Void -> { UfoTron.SetCurrentState(new AStateHostGame());});
-		joinButton = new Button("button.png", new Vector2f(270, 290), new Vector2f(100, 100), Void -> { UfoTron.SetCurrentState(new AStateJoinGame());});
-	
+		try {
+			System.out.println("Menu");
+			UfoTron.isSingleGame = false;
+			hostButton = new Button("host_UfoTron_off.png", new Vector2f(270, 190), new Vector2f(100, 100), Void -> { UfoTron.SetCurrentState(new AStateHostGame());});
+			joinButton = new Button("join_UfoTron_off.png", new Vector2f(270, 290), new Vector2f(100, 100), Void -> { UfoTron.SetCurrentState(new AStateJoinGame());});
+			titleImage = new Image("title.png");
+		} catch (SlickException ex) {
+			Logger.getLogger(AStateMenu.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 	
 	@Override
@@ -37,7 +47,7 @@ public class AStateMenu extends ApplicationState
 	{
 		hostButton.Render();
 		joinButton.Render();
-		
+		titleImage.draw(0, 0, container.getWidth(), container.getHeight()*(float)0.4);
 		iPServer.render(container, g);
 	}
 }
